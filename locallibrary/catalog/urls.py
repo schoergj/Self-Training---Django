8183a3a8a25e3334c7155081f85a9546bfdfe7f9
schoergj <j.schoergenhumer@gmail.com>
2024-09-26 15:03:48 +0200

    Lecture5 finished

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views # "." is relative import from last package, i.e. path from django.urls
 
 urlpatterns = [
-    
+    path("", views.index, name="index"),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+    
+    # counter for genres "
+    num_genre = Genre.objects.count()
+
+    # number of books containing "schwarm"
+    num_books_schwarm = Book.objects.filter(title__icontains="schwarm").count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        "num_genres": num_genre,
+        "num_schwarm": num_books_schwarm,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
