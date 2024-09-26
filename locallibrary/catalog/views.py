aaa0cc112aa072978aa80ed51adfc79e063c95e1
schoergj <j.schoergenhumer@gmail.com>
2024-09-26 17:15:51 +0200

    lession 6 completed

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -34,3 +34,24 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+# define class-based view
+from django.views import generic
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
