710a95a76367f8bed505141a6b0a535fba51503d
schoergj <j.schoergenhumer@gmail.com>
2024-10-01 16:33:29 +0200

    lesson 9m challenge not yet completed

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -17,3 +17,13 @@ urlpatterns += [
 urlpatterns += [
     path('allbooks/', views.LoanedBooksStaffView.as_view(), name='all-borrowed-books'),
 ]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
